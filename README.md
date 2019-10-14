0e501b1eb1609f71f3217c118d921c2541244e12
ncjllld <ncjllld@pc>
2019-10-14 21:43:02 +0800

    Add setup file.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # BYASE
+
+A library that uses Bayesian inference to identify gene-level 
+and isoform-level ASE (Allele-specific expression) in polyploid 
+(diploid or higher) organisms from single-end or 
+paired-end RNA-seq data.
---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="byase",
+    version="1.0.1",
+    author="Lili Dong",
+    author_email="ncjllld@hit.edu.cn",
+    description="A library that uses Bayesian inference to identify gene-level and isoform-level ASE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ncjllld/byase",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'pymc3',
+        'htseq',
+        'pyarrow'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'byase=byase.run:main'],
+    },
+)
